9ea8f0990ddf37c03664e0386b3fc5082bff31bc
Cronos <kabila.desalgn@gmail.com>
2021-12-23 13:38:07 +0300

    finised with slider

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/movie';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  styleUrls: ['./slider.component.scss'],
+  animations: [
+    trigger('fade', [
+      state(
+        'void',
+        style({
+          opacity: 0,
+        })
+      ),
+      transition('void <=> *', [animate('1s')]),
+    ]),
+  ],
 })
 export class SliderComponent implements OnInit {
-
-  constructor() { }
+  @Input() items: Movie[] = [];
+  currentSlide = 0;
 
   ngOnInit(): void {
+    setInterval(() => {
+      this.currentSlide = ++this.currentSlide % this.items.length;
+    }, 5600);
   }
-
 }
